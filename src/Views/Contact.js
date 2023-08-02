588ec8accaeee6d80616747eec5592bb4cfb1f9b
Ruma <matteoh8@hotmail.it>
2023-08-02 12:03:09 +0200

    error page

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Contact = () => {
-  return (
-    <>
-            <h1>Contact</h1>
-            <form>
-                <label>Your name</label>
-                <input type="text"></input>
-                <button type='submit'>Contact us!</button>
+function ContactForm() {
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Name: ${name}, Message: ${message}`);
+        // Qui potresti ad esempio inviare i dati del form al tuo server
+    }
+
+    return (
+        <div className="container mt-4">
+            <h1>Contact us</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Your name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Your message</label>
+                    <textarea
+                        className="form-control"
+                        value={message}
+                        onChange={handleMessageChange}
+                    />
+                </div>
+                <button type='submit' className="btn btn-primary mt-2">Contact us!</button>
             </form>
-        </>
-  )
+        </div>
+    );
 }
 
-export default Contact;
+export default ContactForm;
---
+++ src/Views/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+    return (<>
+        <h2>404 - Pagina non trovata.</h2>
+        <a href="/" className='btn btn-primary'>
+            Torna alla homepage</a>
+    </>)
+}
+
+export default ErrorPage;
+
