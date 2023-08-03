f7b3a36e3f3fe9294412e768390d2fff7a5030a9
Ruma <matteoh8@hotmail.it>
2023-08-03 11:18:25 +0200

    aggiunti NavLink

--- src/App.js
+++ src/App.js
@@ -5,23 +5,20 @@ import Contact from './Views/Contact';
 import Home from './Views/Home';
 import ErrorPage from './Views/ErrorPage';
 import React from 'react';
+import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App container-fluid py-2">
             <h1 className='fw-bold'>App.js</h1>
-            {/* <nav className='d-flex gap-2 justify-content-center'>
-                <a href="/">Homepage</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-            </nav> */}
             <Router>
-                <nav className='d-flex gap-2 justify-content-center'>
+                <Navbar />
+                {/* <nav className='d-flex gap-2 justify-content-center'>
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Contact</Link>
-                </nav>
+                </nav> */}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
