11bed17abb3c92db47e6b01a4b617f6fc0e78f29
Ruma <matteoh8@hotmail.it>
2023-08-05 10:13:48 +0200

    navigazione in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate = useNavigate();
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
@@ -11,24 +12,33 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
+                            <NavLink onClick={() => navigate(-1)}
+                                className='nav-link'>Indietro</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink onClick={() => navigate(1)}
+                                className='nav-link'>Avanti</NavLink>
+                        </li>
+                        <li className="nav-item">
                             <NavLink className="nav-link active" aria-current="page" to="/">
                                 Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink to="/about"
                                 className={({ isActive, isPending }) =>
-                                    isPending ? "pending" : isActive ? "active-link" : ""}>
+                                    isPending ? "pending" : isActive ? "active-link" : "nav-link"}>
                                 About</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink to="/contact"
                                 style={({ isActive, isPending }) => {
-                                return {
-                                    color: isPending ? "blue" : "black",
-                                    fontWeight: isActive ? "bold" : "",
-                                    fontSize: isActive ? "1.2em" : "",
-                                    color: isActive ? "red" : "black",
-                                };}}>Contact</NavLink>
+                                    return {
+                                        color: isPending ? "blue" : "black",
+                                        fontWeight: isActive ? "bold" : "",
+                                        fontSize: isActive ? "1.2em" : "",
+                                        color: isActive ? "red" : "black",
+                                    };
+                                }}>Contact</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="dashboard/login" exact>
--- src/Views/ErrorPage.js
+++ src/Views/ErrorPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 const ErrorPage = () => {
     const backToHomepage = useNavigate();
+    const navigate = useNavigate();
     return (<>
         <h2>404 - Pagina non trovata.</h2>
+        <button onClick={()=> navigate(-1)}
+            className='btn btn-success me-1'>Torna indietro</button>
         <button onClick={() => backToHomepage("/")}
-            className='btn btn-primary'>
-            Torna alla homepage</button>
+            className='btn btn-primary'>Torna alla homepage</button>
     </>)
 }
 
